e6a496cd1294a04fdf51ab69d19c924102e5e9ca
jeremy carrasquilla <jeremycarrasquilla@gmail.com>
2019-10-29 20:59:11 -0500

    lista y todo bien bonito

---
+++ src/personaconlista/Personaconlista.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package personaconlista;
+
+/**
+ *
+ * @author Amaromix
+ */
+public class Personaconlista {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Boton v1=new Boton();
+        
+        v1.setVisible(true);
+    }
+    
+}
